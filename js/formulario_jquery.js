8d7026d8f665dda8965b7d53010eb3447b8b13ca
jorgemochonbernalfullstack <jorgemochonbernallopez@gmail.com>
2023-12-17 00:26:24 +0100

    mostar disponibilidad

--- js/formulario_jquery.js
+++ js/formulario_jquery.js
@@ -10,18 +10,13 @@ $(function () {
   }
 
   function obtenerDatos(e) {
-    const clienteNombreInput = $("#cliente_nombre");
-    const clienteNombreLabel = $('label[for="cliente_nombre"]');
     const esNombreValido = validarCampo(
-      clienteNombreInput.val(),
-      clienteNombreLabel
+      $("#cliente_nombre").val(),
+      $('label[for="cliente_nombre"]')
     );
-
-    const clienteApellidoInput = $("#cliente_apellido");
-    const clienteApellidoLabel = $('label[for="cliente_apellido"]');
     const esApellidoValido = validarCampo(
-      clienteApellidoInput.val(),
-      clienteApellidoLabel
+      $("#cliente_apellido").val(),
+      $('label[for="cliente_apellido"]')
     );
 
     if (!esNombreValido || !esApellidoValido) {
@@ -31,12 +26,35 @@ $(function () {
     }
   }
 
-  function eventos() {
-    const formulario = $("#formulario");
-    formulario.submit(function (e) {
-      obtenerDatos(e);
+  function mostrarDisponibilidadSize(e) {
+    let sizeSelected = $(e.target).val();
+    $.ajax({
+      type: "POST",
+      url: "http://127.0.0.1:5000/checksize",
+      data: {
+        size: sizeSelected
+      },
+      success: (result) => {
+        if (typeof result === 'string') {
+          $('#is_disponible').text(result);
+        } else {
+          console.error("El resultado no es una cadena", result);
+        }
+        return;
+      },
+      error: (error) => {
+        console.error("Error en la llamada Ajax", error);
+      }
     });
   }
 
+  function eventos() {
+    $("#formulario").on('submit', (e) => {
+      obtenerDatos(e);
+    });
+    $('#size').on('change', (e) => {
+      mostrarDisponibilidadSize(e)
+    });
+  }
   eventos();
 });
